e622f74fe8fecbe0d366a1088de0b8633ad886b5
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2019-03-07 15:56:25 +0700

    Remove static css import

--- examples/with-next/pages/_document.js
+++ examples/with-next/pages/_document.js
@@ -1,39 +1,26 @@
-import Document, { Head, Main, NextScript } from 'next/document'
-import flush from 'styled-jsx/server'
+import Document, { Head, Main, NextScript } from "next/document";
+import flush from "styled-jsx/server";
 
 export default class MyDocument extends Document {
-  static getInitialProps ({ renderPage }) {
-    const { html, head, errorHtml, chunks } = renderPage()
-    const styles = flush()
-    return { html, head, errorHtml, chunks, styles }
+  static getInitialProps({ renderPage }) {
+    const { html, head, errorHtml, chunks } = renderPage();
+    const styles = flush();
+    return { html, head, errorHtml, chunks, styles };
   }
 
-  render () {
-    const { __NEXT_DATA__ } = this.props
+  render() {
+    const { __NEXT_DATA__ } = this.props;
 
     return (
       <html>
         <Head>
-          {process.env.NODE_ENV === 'development' ? (
-            <link
-              rel='stylesheet'
-              type='text/css'
-              href='/static/assets/index.css'
-            />
-          ) : (
-            <link
-              rel='stylesheet'
-              type='text/css'
-              href={`/static/assets/${__NEXT_DATA__.buildId}.css`}
-            />
-          )}
-          <link rel='shortcut icon' href={require('../static/favicon.png')} />
+          <link rel="shortcut icon" href={require("../static/favicon.png")} />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </html>
-    )
+    );
   }
 }
