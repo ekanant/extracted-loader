ccfee8b7fa365dcc4d59e1488a7620ca318415f3
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2019-03-07 15:23:27 +0700

    Update dependencies and change to user mini-css-extract-plugin

--- examples/with-next/next.config.js
+++ examples/with-next/next.config.js
@@ -1,6 +1,6 @@
-const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const exportPathMap = require('./routes').exportPathMap
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const exportPathMap = require("./routes").exportPathMap;
 
 module.exports = {
   webpack: (config, { buildId, dev }) => {
@@ -8,75 +8,73 @@ module.exports = {
       test: /\.((sa|sc|c)ss|jpg|png)$/,
       use: [
         {
-          loader: 'emit-file-loader',
+          loader: "emit-file-loader",
           options: {
-            name: 'dist/[path][name].[ext]'
+            name: "dist/[path][name].[ext]"
           }
         }
       ]
-    })
+    });
 
     config.module.rules.push({
       test: /\.(jpg|png|svg)$/,
       use: [
         {
-          loader: 'url-loader',
+          loader: "url-loader",
           options: {
             limit: 10000,
-            name: '.static/assets/[hash].[ext]',
-            outputPath: dev ? path.join(__dirname, '/') : undefined,
-            publicPath: function (url) {
-              return url.replace(/^.*.static/, '/static')
+            name: ".static/assets/[hash].[ext]",
+            outputPath: dev ? path.join(__dirname, "/") : undefined,
+            publicPath: function(url) {
+              return url.replace(/^.*.static/, "/static");
             }
           }
         }
       ]
-    })
+    });
 
     config.module.rules.push({
       test: /\.(sa|sc|c)ss$/,
-      use: ['extracted-loader'].concat(
-        ExtractTextPlugin.extract({
-          use: [
-            'babel-loader',
-            {
-              loader: 'css-loader',
-              options: {
-                url: true,
-                minimize: !dev,
-                sourceMap: dev,
-                importLoaders: 2
-              }
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                plugins: [
-                  require('autoprefixer')({
-                    /* options */
-                  })
-                ]
-              }
-            },
-            {
-              loader: 'sass-loader'
-            }
-          ]
-        })
-      )
-    })
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader
+        },
+        "babel-loader",
+        {
+          loader: "css-loader",
+          options: {
+            url: true,
+            sourceMap: dev,
+            importLoaders: 2
+          }
+        },
+        {
+          loader: "postcss-loader",
+          options: {
+            plugins: [
+              require("autoprefixer")({
+                /* options */
+              })
+            ]
+          }
+        },
+        {
+          loader: "sass-loader"
+        }
+      ]
+    });
 
     config.plugins.push(
-      new ExtractTextPlugin({
+      new MiniCssExtractPlugin({
         filename: dev
-          ? path.join(__dirname, '.static/assets/index.css')
-          : '.static/assets/' + buildId + '.css',
+          ? "static/chunks/index.css"
+          : "static/chunks/" + buildId + ".css",
         allChunks: true
       })
-    )
+    );
 
-    return config
+    return config;
   },
   useFileSystemPublicRoutes: false,
   exportPathMap
-}
+};
